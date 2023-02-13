f09667d45cfa22e19186dce6f06a295a126d8ec0
Karim <karimaboshamia@gmail.com>
2023-02-14 00:54:29 +0200

    Update

--- src/controllers/system-product-controller.ts
+++ src/controllers/system-product-controller.ts
@@ -168,6 +168,7 @@ const getPharmacyProducts = async (req: ExpRequest, res: ExpResponse, next: ExpN
     let productsImages = [];
     let counter = 0;
     for (let pr of products) {
+        if(!pr.product[0].images) continue;
         let imagesUrls = await generateProductImagesURL(pr.product[0].images);
         productsImages.push(imagesUrls);
         pr.product[0].images = mapProductImages(pr.product[0].images, productsImages[counter]);

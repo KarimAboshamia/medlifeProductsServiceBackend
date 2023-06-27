71abeb35b876c5ed76700d756b4883f7cb8530a0
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-06-27 15:06:53 +0200

    Update main.spec.ts

--- src/test/main.spec.ts
+++ src/test/main.spec.ts
@@ -2,6 +2,7 @@ import { describe, before } from 'mocha';
 
 import '../app';
 import Product from '../models/admin-product-model';
+import PharmacyProduct from '../models/pharmacy-product-model';
 
 describe('Mobile Service', function () {
     const NODE_ENV = String(process.env.NODE_ENV)?.trim();
@@ -14,7 +15,10 @@ describe('Mobile Service', function () {
 
     before(async function () {
         await Product.deleteMany();
+        await PharmacyProduct.deleteMany();
     });
 
     require('./admin-products/admin-products.spec');
+    require('./pharmacy-products/pharmacy-products.spec');
+    require('./system-products/system-products.spec');
 });

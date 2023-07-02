974afd037443efd35f8d7a2141ddbf69e343e414
karimaboshamia <karimaboshamia@gmail.com>
2023-07-02 16:26:18 +0300

    Logging

--- src/utilities/receiving-message-broker-utility.ts
+++ src/utilities/receiving-message-broker-utility.ts
@@ -18,6 +18,7 @@ export const pullMessageFromQueue = async (
     controller: (data: any) => Promise<any>
 ) => {
     try {
+        console.log("Consuming");
         channel.consume(
             queueName,
             async (msg) => {
@@ -37,6 +38,7 @@ export const pullMessageFromQueue = async (
                     };
                 }
 
+                console.log("Sending back the response");
                 await channel.sendToQueue(msg?.properties.replyTo!, Buffer.from(JSON.stringify(res)), {
                     correlationId: msg?.properties.correlationId,
                 });
@@ -50,13 +52,16 @@ export const pullMessageFromQueue = async (
 
 export async function createChannel(): Promise<{ channel: amqp.Channel }> {
     try {
+        console.log("connect to broker URL")
         const connection = await amqp.connect(BROKER_URL);
+        console.log("create channel")
         let channel = await connection.createChannel();
 
         const receivingChannel = ReceivingChannelSingleton.getInstance();
 
         receivingChannel.channel = channel;
 
+        console.log("reopen channel")
         //when connection is closed reopen channel
         channel.on('error', async () => {
             // Re-open the channel
@@ -74,6 +79,7 @@ export async function createChannel(): Promise<{ channel: amqp.Channel }> {
 
 export async function createQueue(queueName: string, channel: amqp.Channel) {
     try {
+        console.log("create queue")
         await channel.assertQueue(queueName, { durable: false });
         channel.prefetch(1);
     } catch (error) {
@@ -85,37 +91,51 @@ export const callReceiver = async () => {
     try {
         const receivingChannel = ReceivingChannelSingleton.getInstance();
 
+        console.log("calling create channel");
         await createChannel();
 
+        
+        console.log("calling create queue 1");
         await createQueue(DEL_PHARMACY_PRODUCTS_QUEUE, receivingChannel.channel);
+
+        console.log("calling pull message from queue 1");
         await pullMessageFromQueue(
             DEL_PHARMACY_PRODUCTS_QUEUE,
             receivingChannel.channel,
             pharmacyProductBroker.deletePharmacyProducts
         );
 
+        console.log("calling create queue 2");
         await createQueue(GET_PHARMACIES_PRODUCTS_WITH_IDS_QUEUE, receivingChannel.channel);
+
+        console.log("calling pull message from queue 2");
         await pullMessageFromQueue(
             GET_PHARMACIES_PRODUCTS_WITH_IDS_QUEUE,
             receivingChannel.channel,
             pharmacyProductBroker.getPharmaciesProductsWithIds
         );
 
+        console.log("calling create queue 3");
         await createQueue(GET_PRODUCTS_PHARMACIES_QUEUE, receivingChannel.channel);
+        console.log("calling pull message from queue 3");
         await pullMessageFromQueue(
             GET_PRODUCTS_PHARMACIES_QUEUE,
             receivingChannel.channel,
             pharmacyProductBroker.getProductsPharmacies
         );
 
+        console.log("calling create queue 4");
         await createQueue(REDUCE_PHARMACY_PRODUCTS_AMOUNT_IF_POSSIBLE_QUEUE, receivingChannel.channel);
+        console.log("calling pull message from queue 4");
         await pullMessageFromQueue(
             REDUCE_PHARMACY_PRODUCTS_AMOUNT_IF_POSSIBLE_QUEUE,
             receivingChannel.channel,
             pharmacyProductBroker.decreasePharmacyProductsAmountIfPossible
         );
 
+        console.log("calling create queue 5");
         await createQueue(CREATE_NOTIFY_WHEN_AVAILABLE_REQUEST_QUEUE, receivingChannel.channel);
+        console.log("calling pull message from queue 5");
         await pullMessageFromQueue(
             CREATE_NOTIFY_WHEN_AVAILABLE_REQUEST_QUEUE,
             receivingChannel.channel,
